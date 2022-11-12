c8b4c50cad5ce7835edf4e2e8ce7e13164b1daff
ahzero7d1 <113351371+ahzero7d1@users.noreply.github.com>
2022-11-12 16:35:16 +0900

    obstacle script update

---
+++ Assets/script/Obstacle_bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_bird : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    private bool touched = false;
+
+    private void OnEnable() {
+        touched = false;
+
+        for(int i=0; i<obstacles.Length; i++) {
+            if(Random.Range(0, 3) == 0) {
+                obstacles[i].SetActive(true);
+            }
+            else
+            {
+                obstacles[i].SetActive(false);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.collider.tag == "Player" && !touched) {
+            touched = true;
+            //GameManager.instance.AddScore(1);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
